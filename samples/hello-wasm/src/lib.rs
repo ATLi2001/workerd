b49f62528ede96af86abf63f45c3d58771acf7b4
atli <atli@node0.atli-172090.praxis-pg0.utah.cloudlab.us>
2023-10-14 16:33:10 -0600

    workerd local for miniflare

--- samples/hello-wasm/src/lib.rs
+++ samples/hello-wasm/src/lib.rs
@@ -22,17 +22,26 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     // Optionally, use the Router to handle matching endpoints, use ":name" placeholders, or "*name"
     // catch-alls to match on specific patterns. Alternatively, use `Router::with_data(D)` to
     // provide arbitrary data that will be accessible in each route via the `ctx.data()` method.
-    let router = Router::new();
+    // let router = Router::new();
 
     // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
     // functionality and a `RouteContext` which you can use to  and get route parameters and
     // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
-    router
-        .get("/", |_, _| Response::ok("Hello WASM!"))
-        .get("/worker-version", |_, ctx| {
-            let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
-            Response::ok(version)
-        })
-        .run(req, env)
-        .await
+    // router
+    //    .get("/", |_, _| Response::ok("Hello WASM!"))
+    //    .get("/worker-version", |_, ctx| {
+    //        let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
+    //        Response::ok(version)
+    //    })
+    //   .run(req, env)
+    //    .await
+    
+    // let store = env.kv("KV_FROM_RUST")?;
+    // store.put("testKey", "testValue")?.execute().await?;
+
+    // let val = store.get("testKey").text().await?;
+    // console_debug!("{:?}", val.unwrap());
+
+    Response::ok("Hello WASM!")
+
 }
