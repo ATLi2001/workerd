18bf17d3584dd3d92281f0c0b0c99ade368df5a4
atli <atli@node0.atli-172090.praxis-pg0.utah.cloudlab.us>
2023-10-24 09:36:23 -0600

    custom build script for copying workerd binary into wrangler

---
+++ my_workerd_wrangler_build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# possible architectures are linux-64, linux-arm64, darwin-64, darwin-arm64
+LINUX_ARC="linux-64"
+WORKERD_VERSION="1.20231002.0"
+WORKERD_BIN="$HOME/workerd/bazel-bin/src/workerd/server/workerd"
+WRANGLER_WORKERD_DIR="$HOME/workers-sdk/node_modules/.pnpm/@cloudflare+workerd-$LINUX_ARC@$WORKERD_VERSION/node_modules/@cloudflare/workerd-$LINUX_ARC"
+
+# build workerd
+bazel build //src/workerd/server:workerd
+
+# remove existing binary in wrangler
+rm -f "$WRANGLER_WORKERD_DIR/bin/workerd"
+
+# copy in newly built binary
+cp $WORKERD_BIN "$WRANGLER_WORKERD_DIR/bin/workerd"
+
